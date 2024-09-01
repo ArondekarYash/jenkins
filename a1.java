2ee38960f4daef5017005210e8764c3521f14223
ArondekarYash <156115819+ArondekarYash@users.noreply.github.com>
2024-09-01 12:21:52 +0530

    Update a1.java
    3 commit

--- a1.java
+++ a1.java
@@ -1,5 +1,6 @@
 class yash {
     public static void main(String[] args){
         System.out.println("first code in jinken, by yash arondekar and rajeh ingale");
+         System.out.println("done ");
     }
 }
